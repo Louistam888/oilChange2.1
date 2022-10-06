964a002281fd9bfc42897d3b29438cf2d967747a
louistam888 <louiscodes888@gmail.com>
2022-10-06 08:17:26 -0400

    added commas to output. Refractored code to eliminate 50 lines. Corrected minor typos and realigned answer section

--- script.js
+++ script.js
@@ -1,238 +1,181 @@
+//USER  INPUTS 
 const oilChangeStatus = document.querySelector(".oilChangeStatus");
 const remaining = document.querySelector(".remaining");
 const nextChange = document.querySelector(".nextChange");
 
+//FORM START  
 const form = document.querySelector("form");
 form.addEventListener("submit", (event)=> {
   event.preventDefault();
+
+  //remove all fields in answer section
   oilChangeStatus.classList.remove("yup", "nope", "huh"); 
   oilChangeStatus.innerHTML = "";
   remaining.innerHTML = "";
   nextChange.innerHTML = "";
 
+  //query selectors for user selection 
   const unit = document.querySelector("#unit option:checked").value;
   const oilType = document.querySelector("#lastOilType option:checked").value;
   const lastOilChange = document.querySelector("#odoStart").value;
   const currentOdoReading = document.querySelector("#odoEnd").value;  
+
   const mileage = currentOdoReading - lastOilChange;
-  
 
-  const km = {
+  //function to add commas to numbers 
+  const addCommas = (num) => { 
+    return num.toString().replace(/\B(?<!\.\d*)(?=(\d{3})+(?!\d))/g, ","); 
+  }; 
 
-    syntheticOverdue: {
-      "oilChangeClass": "yup", 
-      "oilChangeStatus": "yes", 
-      "remaining": `You are ` + (mileage - 10000) + ` kms overdue for an oil change. Synthetic oil must be changed every 10,000 kms.`,
-      "nextChange": "Get an oil change now. "
-    },
-    syntheticNotOverdue: {
-      "oilChangeClass": "nope",
-      "oilChangeStatus": "No", 
-      "remaining": `Synthetic oil only needs to be changed every 10,000 kms.`,
-      "nextChange": `Your next oil change will be in ` + (10000 - mileage) + ` kms.`
-    },
-    conventionalOverdue: {
-      "oilChangeClass": "yup", 
-      "oilChangeStatus": "yes", 
-      "remaining": `You are ` + (mileage - 5000) + ` kms overdue for an oil change. Conventional oil must be changed every 5,000 kms.`,
-      "nextChange": "Get an oil change now. "
-    }, 
-    conventionalNotOverdue: {
-      "oilChangeClass": "nope", 
-      "oilChangeStatus": "No", 
-      "remaining": `Conventional oil only needs to be changed every 5,000 kms.`,
-      "nextChange": `Your next oil change will be in ` + (5000 - mileage) + ` kms.`
-    },
+  console.log(addCommas(100000 - mileage))
   
-    noIdeaOverdue: {
-      "oilChangeClass": "yup", 
-      "oilChangeStatus": "yes", 
-      "remaining": `Since you don't know what kind of oil is in your car, we will presume it is conventional oil. Conventional oil must be changed every 5,000 kms. You may be ` + (mileage - 5000) + ` kms overdue for an oil change."`, 
-      "nextChange": `Get an oil change now.`
-    },
-  
-    noIdeaNotOverdue: {
-      "oilChangeClass": "nope",
-      "oilChangeStatus": "No", 
-      "remaining": `Since you don't know what kind of oil is in your car, we will presume it is conventional oil. Conventional oil only needs to be changed every 5,000 kms.`,
-      "nextChange": `Your next oil change will be in ` + (5000 - mileage) + ` kms.`
-    }
+  //function to remove commas from numbers 
+  const removeCommas = (num) => {
+    const removed = parseFloat('100,000.00'.replace(/,/g, ''))
+    return removed
   }
   
-  const miles = {
-
-    syntheticOverdue: {
-      "oilChangeClass": "yup", 
-      "oilChangeStatus": "yes", 
-      "remaining": `You are ` + (mileage - 6200) + ` miles overdue for an oil change. Synthetic oil must be changed every 6,200 miles.`,
-      "nextChange": "Get an oil change now. "
-    },
-    syntheticNotOverdue: {
-      "oilChangeClass": "nope", 
-      "oilChangeStatus": "No", 
-      "remaining": `Synthetic oil only needs to be changed every 6,200km.`,
-      "nextChange": `Your next oil change will be in ` + (6200 - mileage) + ` miles.`
-    },
-    conventionalOverdue: {
-      "oilChangeClass": "yup", 
-      "oilChangeStatus": "yes", 
-      "remaining": `You are ` + (mileage - 3100) + ` miles overdue for an oil change. Conventional oil must be changed every 3,100 miles.`,
-      "nextChange": "Get an oil change now. "
-    }, 
-    conventionalNotOverdue: {
-      "oilChangeClass": "nope", 
-      "oilChangeStatus": "No", 
-      "remaining": `Conventional oil only needs to be changed every 3,100 miles.`,
-      "nextChange": `Your next oil change will be in ` + (3100 - mileage) + ` miles.`
-    },
-    noIdeaOverdue: {
-      "oilChangeClass": "yup", 
-      "oilChangeStatus": "yes", 
-      "remaining": `Since you don't know what kind of oil is in your car, we will presume it is conventional oil. Conventional oil must be changed every 3,100kms. You may be ` + (mileage - 3100) + ` miles overdue for an oil change.`, 
-      "nextChange": `Get an oil change now.`
-    },
-    noIdeaNotOverdue: {
-      "oilChangeClass": "nope",
-      "oilChangeStatus": "No", 
-      "remaining": `Since you don't know what kind of oil is in your car, we will presume it is conventional oil. Conventional oil only needs to be changed every 3,100 miles.`,
-      "nextChange": `Your next oil change will be in ` + (3100 - mileage) + ` miles.`
-    }
-  }
-  
+  //metric function for determining if oil change is needed 
   const metric = ()  => {
     if (mileage >=0 && mileage <= 5000) {
   
       if(oilType === "synthetic") {
-        oilChangeStatus.classList = km.syntheticNotOverdue.oilChangeClass;
-        oilChangeStatus.innerHTML = km.syntheticNotOverdue.oilChangeStatus;
-        remaining.innerHTML = km.syntheticNotOverdue.remaining;
-        nextChange.innerHTML = km.syntheticNotOverdue.nextChange;
+        oilChangeStatus.classList = "nope";
+        oilChangeStatus.innerHTML = "no";
+        remaining.innerHTML = `Synthetic oil only needs to be changed every 10,000 kms.`;
+        nextChange.innerHTML = `Your next oil change will be in ` + (addCommas(10000 - mileage)) + ` kms.`
   
       } else if (oilType === "conventional") {
-        oilChangeStatus.classList = km.conventionalNotOverdue.oilChangeClass;
-        oilChangeStatus.innerHTML = km.conventionalNotOverdue.oilChangeStatus;
-        remaining.innerHTML = km.conventionalNotOverdue.remaining;
-        nextChange.innerHTML = km.conventionalNotOverdue.nextChange;
+        oilChangeStatus.classList = "nope";
+        oilChangeStatus.innerHTML = "no";
+        remaining.innerHTML = `Conventional oil only needs to be changed every 5,000 kms.`;
+        nextChange.innerHTML = `Your next oil change will be in ` + (addCommas(5000 - mileage)) + ` kms.`;      
+
       } else if (oilType === "noIdea") {
-        oilChangeStatus.classList = km.noIdeaNotOverdue.oilChangeClass;
-        oilChangeStatus.innerHTML = km.noIdeaNotOverdue.oilChangeStatus;
-        remaining.innerHTML = km.noIdeaNotOverdue.remaining;
-        nextChange.innerHTML = km.noIdeaNotOverdue.nextChange;
+        oilChangeStatus.classList = "nope";
+        oilChangeStatus.innerHTML = "No";
+        remaining.innerHTML = `Since you don't know what kind of oil is in your car, we will presume it is conventional oil. Conventional oil only needs to be changed every 5,000 kms.`;
+        nextChange.innerHTML = `Your next oil change will be in ` + (addCommas(5000 - mileage)) + ` kms.`;
       }
 
     } else if (mileage >=5000 && mileage <= 10000) {
       
       if(oilType === "synthetic") {
-        oilChangeStatus.classList = km.syntheticNotOverdue.oilChangeClass;
-        oilChangeStatus.innerHTML = km.syntheticNotOverdue.oilChangeStatus;
-        remaining.innerHTML = km.syntheticNotOverdue.remaining;
-        nextChange.innerHTML = km.syntheticNotOverdue.nextChange;
+        oilChangeStatus.classList = "nope";
+        oilChangeStatus.innerHTML = "no";
+        remaining.innerHTML = `Synthetic oil only needs to be changed every 10,000 kms.`,
+        nextChange.innerHTML = `Your next oil change will be in ` + (addCommas(10000 - mileage)) + ` kms.`
   
       } else if (oilType === "conventional") {
-        oilChangeStatus.classList = km.conventionalOverdue.oilChangeClass;
-        oilChangeStatus.innerHTML = km.conventionalOverdue.oilChangeStatus;
-        remaining.innerHTML = km.conventionalOverdue.remaining;
-        nextChange.innerHTML = km.conventionalOverdue.nextChange;
+        oilChangeStatus.classList = "yup";
+        oilChangeStatus.innerHTML = "yes";
+        remaining.innerHTML = `You are ` + (addCommas(mileage - 5000)) + ` kms overdue for an oil change. Conventional oil must be changed every 5,000 kms.`;
+        nextChange.innerHTML = "Get an oil change now."
+
       } else if (oilType === "noIdea") {
-        oilChangeStatus.classList = km.noIdeaOverdue.oilChangeClass;
-        oilChangeStatus.innerHTML = km.noIdeaOverdue.oilChangeStatus;
-        remaining.innerHTML = km.noIdeaOverdue.remaining;
-        nextChange.innerHTML = km.noIdeaOverdue.nextChange;
+        oilChangeStatus.classList = "yup";
+        oilChangeStatus.innerHTML = "yes";
+        remaining.innerHTML = `Since you don't know what kind of oil is in your car, we will presume it is conventional oil. Conventional oil must be changed every 5,000 kms. You may be ` + (addCommas(mileage - 5000))+ ` kms overdue for an oil change.`;
+        nextChange.innerHTML = `Get an oil change now.`
       }
 
     } else if (mileage > 10000) {
 
       if(oilType === "synthetic") {
-        oilChangeStatus.classList = km.syntheticOverdue.oilChangeClass;
-        oilChangeStatus.innerHTML = km.syntheticOverdue.oilChangeStatus;
-        remaining.innerHTML = km.syntheticOverdue.remaining;
-        nextChange.innerHTML = km.syntheticOverdue.nextChange;
+        oilChangeStatus.classList = "yup";
+        oilChangeStatus.innerHTML = "yes";
+        remaining.innerHTML = `You are ` + (addCommas(mileage - 10000)) + ` kms overdue for an oil change. Synthetic oil must be changed every 10,000 kms.`;
+        nextChange.innerHTML = "Get an oil change now. "
   
       } else if (oilType === "conventional") {
-        oilChangeStatus.classList = km.conventionalOverdue.oilChangeClass;
-        oilChangeStatus.innerHTML = km.conventionalOverdue.oilChangeStatus;
-        remaining.innerHTML = km.conventionalOverdue.remaining;
-        nextChange.innerHTML = km.conventionalOverdue.nextChange;
+        oilChangeStatus.classList = "yup";
+        oilChangeStatus.innerHTML = "yes";
+        remaining.innerHTML = `You are ` + (addCommas(mileage - 5000)) + ` kms overdue for an oil change. Conventional oil must be changed every 5,000 kms.`;
+        nextChange.innerHTML = "Get an oil change now."
+
 
       } else if (oilType === "noIdea") {
-        oilChangeStatus.classList = km.noIdeaOverdue.oilChangeClass;
-        oilChangeStatus.innerHTML = km.noIdeaOverdue.oilChangeStatus;
-        remaining.innerHTML = km.noIdeaOverdue.remaining;
-        nextChange.innerHTML = km.noIdeaOverdue.nextChange;
+        oilChangeStatus.classList = "yup";
+        oilChangeStatus.innerHTML = "yes";
+        remaining.innerHTML = `Since you don't know what kind of oil is in your car, we will presume it is conventional oil. Conventional oil must be changed every 5,000 kms. You may be ` + (addCommas(mileage - 5000)) + ` kms overdue for an oil change.`;;
+        nextChange.innerHTML = `Get an oil change now.`
       }
     }
   }
 
+  //imperial function for determining if oil change is needed
   const imperial = () => {
     if (mileage >= 0 && mileage <= 3100) {
   
       if(oilType === "synthetic") {
         
-        oilChangeStatus.classList = miles.syntheticNotOverdue.oilChangeClass;
-        oilChangeStatus.innerHTML = miles.syntheticNotOverdue.oilChangeStatus;
-        remaining.innerHTML = miles.syntheticNotOverdue.remaining;
-        nextChange.innerHTML = miles.syntheticNotOverdue.nextChange; 
+        oilChangeStatus.classList = "nope";
+        oilChangeStatus.innerHTML = "no";
+        remaining.innerHTML = `Synthetic oil only needs to be changed every 6,200 miles.`
+        nextChange.innerHTML = `Your next oil change will be in ` + (addCommas(6200 - mileage)) + ` miles.`
   
       } else if (oilType === "conventional") {
 
-        oilChangeStatus.classList = miles.conventionalNotOverdue.oilChangeClass;
-        oilChangeStatus.innerHTML = miles.conventionalNotOverdue.oilChangeStatus;
-        remaining.innerHTML = miles.conventionalNotOverdue.remaining;
-        nextChange.innerHTML = miles.conventionalNotOverdue.nextChange;
+        oilChangeStatus.classList = "nope";
+        oilChangeStatus.innerHTML = "no";
+        remaining.innerHTML = `Conventional oil only needs to be changed every 3,100 miles.`;
+        nextChange.innerHTML = `Your next oil change will be in ` + (addCommas(3100 - mileage)) + ` miles.`;
+
 
       } else if (oilType === "noIdea") {
 
-        oilChangeStatus.classList = miles.noIdeaNotOverdue.oilChangeClass;
-        oilChangeStatus.innerHTML = miles.noIdeaNotOverdue.oilChangeStatus;
-        remaining.innerHTML = miles.noIdeaNotOverdue.remaining;
-        nextChange.innerHTML = miles.noIdeaNotOverdue.nextChange;
+        oilChangeStatus.classList = "nope";
+        oilChangeStatus.innerHTML = "no";
+        remaining.innerHTML = `Since you don't know what kind of oil is in your car, we will presume it is conventional oil. Conventional oil only needs to be changed every 3,100 miles.`,
+        nextChange.innerHTML = `Your next oil change will be in ` + (addCommas(3100 - mileage)) + ` miles.`;
       }
 
     } else if (mileage >=3100 && mileage <= 6200) {
       
       if(oilType === "synthetic") {
-        oilChangeStatus.classList = miles.syntheticNotOverdue.oilChangeClass;
-        oilChangeStatus.innerHTML = miles.syntheticNotOverdue.oilChangeStatus;
-        remaining.innerHTML = miles.syntheticNotOverdue.remaining;
-        nextChange.innerHTML = miles.syntheticNotOverdue.nextChange;
+        oilChangeStatus.classList = "nope";
+        oilChangeStatus.innerHTML = "no";
+        remaining.innerHTML = `Synthetic oil only needs to be changed every 6,200 miles.`
+        nextChange.innerHTML = `Your next oil change will be in ` + (addCommas(6200 - mileage)) + ` miles.`
   
       } else if (oilType === "conventional") {
-        oilChangeStatus.classList = miles.conventionalOverdue.oilChangeClass;
-        oilChangeStatus.innerHTML = miles.conventionalOverdue.oilChangeStatus;
-        remaining.innerHTML = miles.conventionalOverdue.remaining;
-        nextChange.innerHTML = miles.conventionalOverdue.nextChange;
+        oilChangeStatus.classList = "yup";
+        oilChangeStatus.innerHTML = "yes";
+        remaining.innerHTML = `You are ` + (addCommas(mileage - 3100)) + ` miles overdue for an oil change. Conventional oil must be changed every 3,100 miles.`;
+        nextChange.innerHTML = "Get an oil change now."
  
       } else if (oilType === "noIdea") {
-        oilChangeStatus.classList = miles.noIdeaOverdue.oilChangeClass;
-        oilChangeStatus.innerHTML = miles.noIdeaOverdue.oilChangeStatus;
-        remaining.innerHTML = miles.noIdeaOverdue.remaining;
-        nextChange.innerHTML = miles.noIdeaOverdue.nextChange;
+        oilChangeStatus.classList = "yup";
+        oilChangeStatus.innerHTML = "yes";
+        remaining.innerHTML = `Since you don't know what kind of oil is in your car, we will presume it is conventional oil. Conventional oil must be changed every 3,100 miles. You may be ` + (addCommas(mileage - 3100)) + ` miles overdue for an oil change.`;
+        nextChange.innerHTML = `Get an oil change now.`;
       }
 
     } else if (mileage > 6200) {
 
       if(oilType === "synthetic") {
     
-        oilChangeStatus.classList = miles.syntheticOverdue.oilChangeClass;
-        oilChangeStatus.innerHTML = miles.syntheticOverdue.oilChangeStatus;
-        remaining.innerHTML = miles.syntheticOverdue.remaining;
-        nextChange.innerHTML = miles.syntheticOverdue.nextChange;
+        oilChangeStatus.classList = "yup";
+        oilChangeStatus.innerHTML = "yes";
+        remaining.innerHTML = `You are ` + (addCommas(mileage - 6200)) + ` miles overdue for an oil change. Synthetic oil must be changed every 6,200 miles.`
+        nextChange.innerHTML = "Get an oil change now."
   
       } else if (oilType === "conventional") {
-        oilChangeStatus.classList = miles.conventionalOverdue.oilChangeClass;
-        oilChangeStatus.innerHTML = miles.conventionalOverdue.oilChangeStatus;
-        remaining.innerHTML = miles.conventionalOverdue.remaining;
-        nextChange.innerHTML = miles.conventionalOverdue.nextChange;
+        oilChangeStatus.classList = "yup";
+        oilChangeStatus.innerHTML = "yes";
+        remaining.innerHTML = `You are ` + (addCommas(mileage - 3100)) + ` miles overdue for an oil change. Conventional oil must be changed every 3,100 miles.`;
+        nextChange.innerHTML = "Get an oil change now."
 
       } else if (oilType === "noIdea") {
-        oilChangeStatus.classList = miles.noIdeaOverdue.oilChangeClass;
-        oilChangeStatus.innerHTML = miles.noIdeaOverdue.oilChangeStatus;
-        remaining.innerHTML = miles.noIdeaOverdue.remaining;
-        nextChange.innerHTML = miles.noIdeaOverdue.nextChange;
+        oilChangeStatus.classList = "yup";
+        oilChangeStatus.innerHTML = "yes";
+        remaining.innerHTML = `Since you don't know what kind of oil is in your car, we will presume it is conventional oil. Conventional oil must be changed every 3,100 miles. You may be ` + (addCommas(mileage - 3100)) + ` miles overdue for an oil change.`;;
+        nextChange.innerHTML = `Get an oil change now.`;
       }
     }
   }
 
+  //decision tree function 
   if (mileage < 0) { 
     setTimeout(() => {
       oilChangeStatus.classList = "huh"
@@ -251,7 +194,7 @@ form.addEventListener("submit", (event)=> {
   } 
 });
 
-
+//reset button 
 const resetForm = document.querySelector(".resetButton");
 resetForm.addEventListener("click", (event) => {
   oilChangeStatus.classList.remove("yup", "nope", "huh");
